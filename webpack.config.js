d95940b7718e7bc64a9dcb87184af8c3e5c12a9f
Travis Arnold <ftntravis@gmail.com>
2016-07-13 10:42:11 -0700

    use autoprefixer in local examples

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
   module: {
     loaders: [
       { test: /\.(js|jsx)/, exclude: /node_modules/, loader: 'babel-loader', query: { presets: ['es2015', 'react', 'stage-0'] } },
-      { test: /\.scss$/, loader: 'style!css!sass?sourceMap' }
+      { test: /\.scss$/, loader: 'style!css!autoprefixer!sass?sourceMap' }
     ]
   },
   resolve: {
