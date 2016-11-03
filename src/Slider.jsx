545163fe68ba1a8eb2c5959e32806f22e2c5957d
Travis Arnold <ftntravis@gmail.com>
2016-11-03 13:30:30 -0700

    transitioning to external API

--- src/Slider.jsx
+++ src/Slider.jsx
@@ -6,17 +6,17 @@ const modulo = (num, max) => ((num % max) + max) % max
 // react-view-pager
 
 // const Slider = ({ slides }) => (
-  // <Frame>
-  //   { ({ position, isSliding, isSwiping }) =>
-  //     <Motion style={{ value: isSwiping ? position : spring(position) }}>
-  //       { value =>
-  //         <Track position={value}> // overrides internal position
-  //           {slides} // that position would set proper wrapper values
-  //         </Track>
-  //       }
-  //     </Motion>
-  //   }
-  // </Frame>
+//   <Frame>
+//     { ({ position, isSliding, isSwiping }) =>
+//       <Motion style={{ value: isSwiping ? position : spring(position) }}>
+//         { value =>
+//           <Track position={value}> // overrides internal position
+//             {slides} // that position would set proper wrapper values
+//           </Track>
+//         }
+//       </Motion>
+//     }
+//   </Frame>
 // )
 
 const ALIGN_OFFSETS = {
@@ -25,17 +25,155 @@ const ALIGN_OFFSETS = {
   right: 1
 }
 
-class Slide extends Component {
+class ElementSize {
+  constructor({ node, width, height, axis }) {
+    this.node = node
+    this.axis = axis
+    this.setSize(width, height)
+  }
+
+  setSize(width, height) {
+    this.width = width || this.node.offsetWidth
+    this.height = height || this.node.offsetHeight
+  }
+
+  getSize() {
+    return this[this.axis === 'x' ? 'width' : 'height']
+  }
+}
+
+class View extends ElementSize {
+  constructor(options) {
+    super(options)
+    this.top = this.left = {
+      original: 0,
+      offset: {
+        pixel: 0,
+        percent: 0
+      }
+    }
+  }
+
+  setEdge(position) {
+    this[this.axis === 'y' ? 'top' : 'left'] = position
+  }
+
+  getEdge() {
+    return this[this.axis === 'y' ? 'top' : 'left']
+  }
+}
+
+class Views {
+  constructor(axis) {
+    this.axis = axis
+    this.collection = []
+  }
+
+  setFrame(frame) {
+    this.frame = frame
+  }
+
+  setTrack(track) {
+    this.track = track
+  }
+
+  addView(view) {
+    this.collection.push(view)
+
+    // hydrate positions every time a new view is added
+    this.setPositions()
+  }
+
+  setPositions() {
+    // bail if frame or track haven't been set yet
+    if (!this.frame && !this.track) return
+
+    const frameSize = this.frame.getSize()
+    const trackSize = this.getTotalSize()
+    const trackPosition = this.track.getPosition()
+    const startCoords = { top: 0, left: 0 }
+
+    this.collection.reduce((lastView, view) => {
+      const lastPosition = lastView && lastView.getEdge().original || 0
+      const nextPosition = lastPosition + view.getSize()
+      let offsetPosition = lastPosition
+
+      // offset slides in the proper position when wrapping
+      if (nextPosition < Math.abs(trackPosition)) {
+        offsetPosition += trackSize
+      } else if (lastPosition > (frameSize - trackPosition)) {
+        offsetPosition -= trackSize
+      }
+
+      view.setEdge({
+        original: nextPosition,
+        offset: {
+          pixel: offsetPosition,
+          percent: this.getPercentValue(offsetPosition)
+        }
+      })
+
+      return view
+    }, null)
+  }
+
+  getTotalSize() {
+    let size = 0
+    this.collection.forEach(view => { size += view.width })
+    return size
+  }
+
+  getPercentValue(position) {
+    return Math.round(position / this.frame.getSize() * 10000) * 0.01
+  }
+}
+
+class Track extends ElementSize {
+  constructor(options) {
+    super(options)
+    this.x = this.y = 0
+  }
+
+  setPosition(position) {
+    this[this.axis] = position
+  }
+
+  getPosition() {
+    return this[this.axis]
+  }
+}
+
+class Frame extends ElementSize {
+  constructor(options) {
+    super(options)
+    this.x = this.y = 0
+  }
+
+  setPosition(position) {
+    this[this.axis] = position
+  }
+
+  getPosition() {
+    return this[this.axis]
+  }
+}
+
+
+///////////////////////////////////////////////////////////
+// START REACT
+///////////////////////////////////////////////////////////
+
+class ViewPage extends Component {
   componentDidMount() {
-    this.props.onSlideMount(findDOMNode(this), this.props.index)
+    this.props.onMount(findDOMNode(this), this.props.index)
   }
 
   render() {
-    const { width, position, children } = this.props
+    const { width, position, view, children } = this.props
     const child = Children.only(children)
     const style = {
       ...child.props.style,
-      left: position.left + '%'
+      left: (view.left && view.left.offset.percent) + '%' || 0 + '%'
     }
 
     if (width) {
@@ -45,16 +183,16 @@ class Slide extends Component {
     // pass isCurrent here so we can do stuff with it. Also pass as child function
     // because people may want that functionality
 
-    return cloneElement(child, { style })
+    return cloneElement(child, { ref: this._handleNode, style })
   }
 }
 
-class Slider extends Component {
+class ViewPager extends Component {
   static propTypes = {
     currentKey: PropTypes.any,
     currentIndex: PropTypes.number,
-    slidesToShow: PropTypes.number, // can be false as well
-    slidesToMove: PropTypes.number,
+    // viewsToShow: PropTypes.number, // can be false as well
+    viewsToMove: PropTypes.number,
     infinite: PropTypes.bool,
     instant: PropTypes.bool,
     vertical: PropTypes.bool,
@@ -64,33 +202,34 @@ class Slider extends Component {
     swipe: PropTypes.oneOf([true, false, 'mouse', 'touch']),
     swipeThreshold: PropTypes.number,
     flickTimeout: PropTypes.number,
-    beforeSlide: PropTypes.func,
-    afterSlide: PropTypes.func
+    beforeAnimation: PropTypes.func,
+    afterAnimation: PropTypes.func
   }
 
   static defaultProps = {
-    currentKey: null,
+    // currentKey: null,
     currentIndex: 0,
-    slidesToShow: false,
-    slidesToShow: 3,
-    slidesToMove: 1,
+    viewsToShow: false,
+    viewsToMove: 1,
     infinite: false,
     // instant: false,
-    // vertical: false,
+    axis: 'x',
     // autoHeight: false,
-    align: 'left',
+    align: 'center',
     swipe: true,
     swipeThreshold: 0.5,
     flickTimeout: 300,
     onChange: () => null,
-    beforeSlide: () => null,
-    afterSlide: () => null
+    beforeAnimation: () => null,
+    afterAnimation: () => null
   }
 
   constructor(props) {
     super(props)
 
-    this._slideCount = Children.count(props.children)
+    this._viewCount = Children.count(props.children)
+
+    // swiping
     this._deltaX = false
     this._deltaY = false
     this._startX = {}
@@ -106,19 +245,68 @@ class Slider extends Component {
     }
 
     // NEW
-    this._sliderX = 0
-    this._sliderPosition = 0
-    this._frameWidth = -1
-    this._trackWidth = -1
-    this._slideWidth = -1
-    this._slideDimensions = []
+    this._trackX = 0
+    this._trackPosition = 0
+    this._viewDimensions = []
+
+    if (props.viewsToShow) {
+      this._viewWidth = 100 / props.viewsToShow
+      this._trackWidth = this._viewWidth * this._viewCount
+      this._frameWidth = this._viewWidth * props.viewsToShow
+
+      this._viewHeight = 100 / props.viewsToShow
+      this._trackHeight = this._viewWidth * this._viewCount
+      this._frameHeight = this._viewWidth * props.viewsToShow
+    } else {
+      this._viewWidth = -1
+      this._trackWidth = -1
+      this._frameWidth = -1
+
+      this._viewHeight = -1
+      this._trackHeight = -1
+      this._frameHeight = -1
+    }
+
+    // NEW new
+    this._views = new Views(props.axis)
+  }
+
+  // some type of method that hydrates the variables so it can be used
+  // with a resize detector or whatever
+  hydrate() {
+    const { axis } = this.props
+
+    this._frame = new Frame({
+      node: this._frameNode,
+      axis
+    })
+
+    this._track = new Track({
+      node: this._trackNode,
+      axis
+    })
+
+
+    this._views.setFrame(this._frame)
+    this._views.setTrack(this._track)
+
+    // set positions so we can get a total width
+    this._views.setPositions()
+
+    // set track width to the size of views
+    this._track.setSize(this._views.getTotalSize(), 0)
+
+    if (!this.props.viewsToShow) {
+      this._frameWidth = this._frameNode.offsetWidth
+      this._frameHeight = this._frameNode.offsetHeight
+    }
+
+    this._trackX = this._getLeftStartCoords()
   }
 
   componentDidMount() {
-    this._frameWidth = this._frame.offsetWidth
-    this._slideWidth = (this._frameWidth / this.props.slidesToShow) / this._slideCount
-    this._sliderX = this._getLeftStartCoords()
-    this._setSliderPosition()
+    this.hydrate()
+    this._setTrackPosition()
   }
 
   componentWillReceiveProps({ currentIndex }) {
@@ -127,11 +315,15 @@ class Slider extends Component {
     }
   }
 
+  componentWillUpdate() {
+    this._views.setPositions()
+  }
+
   componentDidUpdate(lastProps, lastState) {
     // reposition slider if index has changed
     if (this.state.currentIndex !== lastState.currentIndex) {
-      this._sliderX = this._getLeftStartCoords()
-      this._setSliderPosition()
+      this._trackX = this._getLeftStartCoords()
+      this._setTrackPosition()
     }
   }
 
@@ -144,15 +336,35 @@ class Slider extends Component {
   }
 
   slide = (direction, index = this.state.currentIndex) => {
-    const currentIndex = modulo(index + direction, this._slideCount)
+    const { viewsToMove } = this.props
+    const currentIndex = modulo(index + (direction * viewsToMove), this._viewCount)
     this.setState({ currentIndex }, () => {
       this.props.onChange(currentIndex)
     })
   }
 
-  _handleSlideMount = (node, index) => {
-    this._slideDimensions.push([node.offsetWidth, node.offsetHeight])
-    this._trackWidth += node.offsetWidth
+  _handleViewMount = (node, index) => {
+    const { viewsToShow, axis } = this.props
+
+    if (viewsToShow) {
+      this._viewDimensions.push([this._viewWidth, this._viewHeight])
+    } else {
+      this._viewDimensions.push([node.offsetWidth, node.offsetHeight])
+      // console.log('track', this._trackWidth)
+      this._trackWidth += node.offsetWidth
+      this._trackHeight += node.offsetHeight
+    }
+
+    ////////////////////////////
+    const view = new View({
+      index,
+      node,
+      axis,
+      track: this._track
+    })
+
+    this._views.addView(view)
+
     this.forceUpdate()
   }
 
@@ -178,7 +390,7 @@ class Slider extends Component {
 
     // store the initial starting coordinates
     this._startX = {
-      slider: this._sliderX,
+      slider: this._trackX,
       page: swipe.pageX
     }
     this._startY = swipe.pageY
@@ -195,7 +407,7 @@ class Slider extends Component {
     // bail if we aren't swiping
     if (!this._isSwiping) return
 
-    const { vertical, swipeThreshold, slidesToMove } = this.props
+    const { vertical, swipeThreshold, viewsToMove } = this.props
     const swipe = e.touches && e.touches[0] || e
 
     // determine how much we have moved
@@ -208,11 +420,11 @@ class Slider extends Component {
 
       const deltaAxis = vertical ? this._deltaY : this._deltaX
 
-      this._sliderX = this._startX.slider - deltaAxis
-      this._setSliderPosition()
+      this._trackX = (this._startX.slider - deltaAxis) * viewsToMove
+      this._setTrackPosition()
 
       // this.setState({
-      //   swipeOffset: (axis / dimension) * slidesToMove,
+      //   swipeOffset: (axis / dimension) * viewsToMove,
       //   instant: true
       // })
     }
@@ -220,8 +432,8 @@ class Slider extends Component {
 
   _onSwipeEnd = () =>  {
     const { swipeThreshold } = this.props
-    const slideWidth = this._getCurrentSlideDimensions()[0]
-    const threshold = this._isFlick ? swipeThreshold : (slideWidth * swipeThreshold)
+    const currentViewSize = this._getCurrentViewSize()
+    const threshold = this._isFlick ? swipeThreshold : (currentViewSize * swipeThreshold)
 
     // this.setState({
     //   swipeOffset: 0,
@@ -267,87 +479,69 @@ class Slider extends Component {
     return swipeEvents
   }
 
-  _getCurrentSlideDimensions() {
-    const { slidesToShow } = this.props
-    return slidesToShow
-      ? [this._slideWidth, 0]
-      : this._slideDimensions[this.state.currentIndex] || [0, 0]
+  _getCurrentViewSize() {
+    const { viewsToShow, vertical } = this.props
+    const slideDimensions = this._viewDimensions[this.state.currentIndex]
+    return viewsToShow
+      ? ( vertical ? this._viewHeight : this._viewWidth )
+      : ( slideDimensions && vertical ? slideDimensions[1] : slideDimensions[0] ) || 0
   }
 
-  _setSliderPosition() {
-    const { align } = this.props
-    const slideWidth = this._getCurrentSlideDimensions()[0]
+  _setTrackPosition() {
+    const { align, vertical } = this.props
     const alignMultiplier = isNaN(align) ? ALIGN_OFFSETS[align] : align
-    let position = modulo(this._sliderX, this._trackWidth) - this._trackWidth
+    const currentViewSize = this._getCurrentViewSize()
+    const trackSize = this._track.getSize()
+    const frameSize = this._frame.getSize()
 
-    position += (this._frameWidth - slideWidth) * alignMultiplier
+    let position = modulo(this._trackX, trackSize) - trackSize
 
-    this._sliderPosition = position
+    position += (frameSize - currentViewSize) * alignMultiplier
+
+    this._trackPosition = position
+
+    this._track.setPosition(position, 0)
 
     this.forceUpdate()
   }
 
   _getPercentValue(position) {
-    return Math.round(position / this._frameWidth * 10000) * 0.01
+    const frameDimension = this.props.vertical ? this._frameHeight : this._frameWidth
+    // console.log('internal position', position)
+    // console.log('INTERNAL TOTAL SIZE', frameDimension)
+    return Math.round(position / frameDimension * 10000) * 0.01
   }
 
   _getLeftStartCoords(index = this.state.currentIndex) {
-    return this._slideDimensions
+    return this._viewDimensions
       .slice(0, index)
-      .reduce((sum, [width]) => sum - width, 0)
-  }
-
-  _getSlidePositions() {
-    const positions = []
-
-    this._slideDimensions.reduce((prevLeft, [origW, origH], i) => {
-      let left = prevLeft
-      let nextLeft = prevLeft + origW
-
-      // offset slides in the proper position when wrapping
-      if (nextLeft < Math.abs(this._sliderPosition)) {
-        left += this._trackWidth
-      } else if (prevLeft > (this._frameWidth - this._sliderPosition)) {
-        left -= this._trackWidth
-      }
-
-      positions.push({
-        // this can be cleaned up, slidesToShow should be moved into a conditional
-        left: this._getPercentValue(left)
-      })
-
-      return nextLeft
-    }, 0)
-
-    return positions
+      .reduce((sum, [width, height]) => sum - (this.props.vertical ? height : width), 0)
   }
 
   render() {
-    const { slidesToShow, children } = this.props
-    const trackPosition = this._getPercentValue(this._sliderPosition)
-    const slidePositions = this._getSlidePositions()
+    const { viewsToShow, children } = this.props
+    const trackPosition = this._getPercentValue(this._trackPosition)
     return (
       <div
-        ref={c => this._frame = findDOMNode(c)}
-        className="gallery"
+        ref={c => this._frameNode = findDOMNode(c)}
+        className="frame"
         {...this._getSwipeEvents()}
       >
         <div
-          ref={c => this._track = findDOMNode(c)}
-          className="slider"
+          ref={c => this._trackNode = findDOMNode(c)}
+          className="track"
           style={{
             transform: `translate3d(${trackPosition}%, 0, 0)`
           }}
         >
           {Children.map(children, (child, index) =>
-            <Slide
+            <ViewPage
               index={index}
-              width={slidesToShow && this._slideWidth}
-              position={slidePositions[index] || { left: 0 }}
-              onSlideMount={this._handleSlideMount}
-            >
-              {child}
-            </Slide>
+              view={this._views.collection[index] || {}}
+              width={viewsToShow && this._viewWidth}
+              onMount={this._handleViewMount}
+              children={child}
+            />
           )}
         </div>
       </div>
@@ -355,4 +549,4 @@ class Slider extends Component {
   }
 }
 
-export default Slider
+export default ViewPager
